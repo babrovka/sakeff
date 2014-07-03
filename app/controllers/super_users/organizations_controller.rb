3dac68b362aa779d7d3ba21ca4197e568e5eca0a
babrovka <babrovka@gmail.com>
2014-07-03 16:28:09 +0400

    strong parameters for organizations

--- app/controllers/super_users/organizations_controller.rb
+++ app/controllers/super_users/organizations_controller.rb
@@ -1,3 +1,9 @@
 class SuperUsers::OrganizationsController < InheritedResources::Base
+  
+  
+  
+  def permitted_params
+    params.permit(:organization => [:legal_status, :short_title, :full_title, :inn])
+  end
 
 end
