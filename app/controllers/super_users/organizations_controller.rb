84472bc095aae5467138ed30784a6a211dbfaa03
justvitalius <justvitalius@gmail.com>
2014-07-04 18:50:15 +0400

    Fix incorrect routing in forms.

--- app/controllers/super_users/organizations_controller.rb
+++ app/controllers/super_users/organizations_controller.rb
@@ -1,9 +1,11 @@
 class SuperUsers::OrganizationsController < SuperUsers::BaseController
   inherit_resources
-  
+
+
   
   def permitted_params
     params.permit(:organization => [:legal_status, :short_title, :full_title, :inn])
   end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   get 'library' => 'library#library'
 
   scope module: 'super_users' do
-    
     scope '/superuser' do
       root 'dashboard#index', as: :super_users_root
       resources :organizations, as: :super_user_organizations
