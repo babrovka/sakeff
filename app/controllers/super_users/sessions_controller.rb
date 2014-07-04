29b3de1856a3ba1449a4d024ec6260d1f077df39
justvitalius <justvitalius@gmail.com>
2014-07-04 21:54:39 +0400

    Fix incorrect redirect after super_user logging.

--- app/controllers/super_users/sessions_controller.rb
+++ app/controllers/super_users/sessions_controller.rb
@@ -2,7 +2,7 @@ class SuperUsers::SessionsController < Devise::SessionsController
   layout 'super_users/sessions'
 
   def after_sign_in_path_for(resource)
-    super_users_root_path
+    super_user_root_path
   end
 
 end
