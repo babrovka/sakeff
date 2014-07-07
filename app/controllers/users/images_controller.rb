966bf8af0e6d419f87d4634247a97c2d98a3b655
babrovka <babrovka@gmail.com>
2014-07-07 12:13:31 +0400

    comment for Users::ImagesController show method

--- app/controllers/users/images_controller.rb
+++ app/controllers/users/images_controller.rb
@@ -1,6 +1,13 @@
 class Users::ImagesController < BaseController
   
+  
+
+  
   def show
+    # отображает изображение пользователя inline 
+    # на выходе имеем изображение заданного типа для заданного пользователя
+    # params[:user] => id'шник пользователя
+    # params[:image_type] => тип изображения: 'page' или 'menu'
     user = User.find(params[:user])
     image_type = params[:image_type]
     image = user.send(image_type)
