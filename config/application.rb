59ccd41aa0bbef7c6cbf0051d277d2b2a83dc784
babrovka <babrovka@gmail.com>
2014-07-08 12:09:43 +0400

    translations

--- config/application.rb
+++ config/application.rb
@@ -15,9 +15,21 @@ module Sakeff
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    
+    config.before_configuration do
+      I18n.config.enforce_available_locales = false #@prdetective TODO: this will default to true in future rails versions
+      I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+      I18n.locale = :ru
+      I18n.default_locale = :ru
+      config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+      config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+      config.i18n.locale = :ru
+      # bypasses rails bug with i18n in production\
+      I18n.reload!
+      config.i18n.reload!
+    end
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
     config.i18n.locale = :ru
 
