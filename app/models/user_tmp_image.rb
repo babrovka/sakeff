d1e86c99e151e03d721cc78c24ab70be47b0d684
babrovka <babrovka@gmail.com>
2014-07-09 12:18:13 +0400

    UserTmpImage crop sizes update

--- app/models/user_tmp_image.rb
+++ app/models/user_tmp_image.rb
@@ -1,5 +1,5 @@
 class UserTmpImage < ActiveRecord::Base
-  has_attached_file :image, :styles => { :menu => ["100x100#",:png], :page => ["180x180#", :png] }, :default_url => "/user_tmp_images/:style/missing.png"
+  has_attached_file :image, :styles => { :menu => ["26x26#",:png], :page => ["100x100#", :png] }, :default_url => "/user_tmp_images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
   belongs_to :user
 end
