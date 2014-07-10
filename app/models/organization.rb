2ca5d1451886a658b868d1ee09df031e95e42008
babrovka <babrovka@gmail.com>
2014-07-10 16:22:09 +0400

    fix organization validation

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,7 +2,8 @@ class Organization < ActiveRecord::Base
   
   validates :legal_status, :short_title, :full_title, :inn, presence: true
   validates :inn, uniqueness: true
-  validates :inn, numericality: { equal_to: 10 }
+  validates :inn, numericality: true
+  validates :inn, length: { is: 10 }
 
   enum legal_status: [:ip, :ooo, :zao, :oao]
   
