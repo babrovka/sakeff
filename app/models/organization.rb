5a0aafbcea36398558b265fb900257ee08eabef0
babrovka <babrovka@gmail.com>
2014-07-10 11:56:23 +0400

    organization inn validation numericality

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,7 +2,7 @@ class Organization < ActiveRecord::Base
   
   validates :legal_status, :short_title, :full_title, :inn, presence: true
   validates :inn, uniqueness: true
-
+  validates :inn, numericality: { equal_to: 10 }
 
   enum legal_status: [:ip, :ooo, :zao, :oao]
   
