cc62c24aa996689e9a5268f55f9fde1608bdc47e
Vladimir Krivchenko <vladimir.krivchenko@gmail.com>
2014-07-17 12:54:29 +0400

    Add CRUD actions for regulations

--- app/controllers/control/regulations_controller.rb
+++ app/controllers/control/regulations_controller.rb
@@ -1,5 +1,18 @@
 class Control::RegulationsController < BaseController
-  def index
-    
-  end
+  inherit_resources
+  actions :all, except: :show
+
+  private
+
+    def root_url
+      control_regulations_url
+    end
+
+    def collection_url
+      control_regulations_url
+    end
+
+    def permitted_params
+      params.permit(control_regulation: [:name])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,10 +22,8 @@ Rails.application.routes.draw do
     end
   end
 
-  scope module: 'control' do
-    scope '/control' do
-        resources :regulations
-    end
+  namespace 'control' do
+    resources :regulations
   end
 
 end
