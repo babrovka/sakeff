cb8a4ebe1da647788783d9d2230b605882dd183b
justvitalius <justvitalius@gmail.com>
2014-07-18 15:00:17 +0400

    Add spec for editing Organization by super user.

--- spec/acceptance/super_users/organizations/manage_organizations_spec.rb
+++ spec/acceptance/super_users/organizations/manage_organizations_spec.rb
@@ -33,5 +33,46 @@ feature "SuperUser manage Organizations", %q() do
     end
 
   end
+
+
+  describe 'Edit exist Organization' do
+
+    let!(:organization) { create(:organization) }
+    let!(:old_organization_inn) { organization.inn }
+
+    background do
+      login_as(super_user, :scope => :super_user)
+      visit edit_super_user_organization_path(organization)
+
+      fill_in 'organization[inn]', with: '0987654321'
+    end
+
+    context 'with valid attributes' do
+      scenario 'success update' do
+        expect { click_on 'Сохранить' }.to_not change(Organization, :count)
+        expect(current_path).to eq super_user_organizations_path
+
+        # проверяем,что параметр изменился в БД
+        organization.reload
+        expect(organization.inn).to_not eq old_organization_inn
+      end
+    end
+
+    context 'without valid attributes' do
+      scenario 'failed update' do
+        # по сценарию, ИНН имеет только 10 цифр,он и будет выступать в роли не верного параметра
+        new_inn = '123456'
+
+        fill_in 'organization[inn]', with: new_inn
+        expect { click_on 'Сохранить' }.to_not change(Organization, :count)
+
+        # проверяем,что параметр не записался в БД
+        organization.reload
+        expect(organization.inn).to_not eq new_inn
+      end
+    end
+
+  end
+
 end
 
