4b9b9b77de4ccf65723f5ec2dfe64c1232115129
babrovka <babrovka@gmail.com>
2014-07-18 14:50:40 +0400

    valiadtion for permission title & role title

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,6 @@
 class Role < ActiveRecord::Base
   validates :title, :description, presence: true
+  validates :title, uniqueness: true
   has_many :role_permissions
   has_many :permissions, through: :role_permissions
   has_many :user_roles
