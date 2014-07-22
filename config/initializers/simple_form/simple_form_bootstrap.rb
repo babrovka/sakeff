9d0e6d40c61af59067468cf78e5aebe3a8e975a5
cbrwizard <cbrwizard@gmail.com>
2014-07-22 13:24:20 +0400

    Styling of role add form

--- config/initializers/simple_form/simple_form_bootstrap.rb
+++ config/initializers/simple_form/simple_form_bootstrap.rb
@@ -60,7 +60,7 @@ SimpleForm.setup do |config|
     end
   end
 
-  config.wrappers :short_field, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+  config.wrappers :short_field, tag: 'div', class: 'col-sm-12', error_class: 'has-error' do |b|
     b.use :html5
     b.use :placeholder
     b.use :label, class: 'col-sm-2 control-label'
@@ -72,7 +72,7 @@ SimpleForm.setup do |config|
     end
   end
 
-  config.wrappers :middle_field, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+  config.wrappers :middle_field, tag: 'div', class: 'col-sm-12', error_class: 'has-error' do |b|
     b.use :html5
     b.use :placeholder
     b.use :label, class: 'col-sm-2 control-label'
@@ -88,7 +88,18 @@ SimpleForm.setup do |config|
     b.use :html5
     b.use :placeholder
 
-    b.wrapper tag: 'div', class: 'col-sm-4' do |ba|
+    b.wrapper tag: 'div', class: 'col-sm-3' do |ba|
+      ba.use :input, class: 'form-control'
+      ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+    end
+  end
+
+  config.wrappers :inline_field_small, tag: 'div', class: 'form-inline', error_class: 'has-error' do |b|
+    b.use :html5
+    b.use :placeholder
+
+    b.wrapper tag: 'div', class: 'col-sm-2' do |ba|
       ba.use :input, class: 'form-control'
       ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
       ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
