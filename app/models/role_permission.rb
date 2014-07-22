b49723f5063545df899f961763533567a0941419
cbrwizard <cbrwizard@gmail.com>
2014-07-22 18:06:41 +0400

    Correct validation of empty fields for role permissions

--- app/models/role_permission.rb
+++ app/models/role_permission.rb
@@ -3,6 +3,9 @@ class RolePermission < ActiveRecord::Base
   belongs_to :permission
   enum result: [ :default, :granted, :forbidden ]
 
+  validates :result, :presence => true
+  validates :permission_id, :presence => true
+
   # validates :role_id, :uniqueness => {:scope => :permission_id, :message => "Нельзя дублировать право у роли"}
 
 end
