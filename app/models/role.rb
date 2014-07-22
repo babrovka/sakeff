feebde43862805bb49f8eecbcf65e211ee957f1d
justvitalius <justvitalius@gmail.com>
2014-07-22 12:07:36 +0400

    Add permission_result method to Role.

--- app/models/role.rb
+++ app/models/role.rb
@@ -4,4 +4,12 @@ class Role < ActiveRecord::Base
   has_many :permissions, through: :role_permissions
   has_many :user_roles
   has_many :users, through: :user_roles
+
+
+
+  def permission_result(permission)
+    if permission && permission.is_a?(Permission)
+      RolePermission.where(role_id: self.id, permission_id: permission.id).first.try(:result)
+    end
+  end
 end
