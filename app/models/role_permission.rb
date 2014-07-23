8cf5430aa3c61f3daff3a5540163677ec0d25b80
cbrwizard <cbrwizard@gmail.com>
2014-07-23 16:26:55 +0400

    Usage of 1.9+ hash syntax

--- app/models/role_permission.rb
+++ app/models/role_permission.rb
@@ -3,6 +3,6 @@ class RolePermission < ActiveRecord::Base
   belongs_to :permission
   enum result: [ :default, :granted, :forbidden ]
 
-  validates :result, :presence => true
-  validates :permission_id, :presence => true
+  validates :result, presence: true
+  validates :permission_id, presence: true
 end
