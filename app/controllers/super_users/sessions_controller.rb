fd13e300dffe2381045c4754c939a38bde54419d
babrovka <babrovka@gmail.com>
2014-07-23 12:47:23 +0400

    log only after su logged out

--- app/controllers/super_users/sessions_controller.rb
+++ app/controllers/super_users/sessions_controller.rb
@@ -2,7 +2,12 @@ class SuperUsers::SessionsController < Devise::SessionsController
   layout 'super_users/sessions'
   
   after_action :super_user_logged_in, only: :create
-  before_action :super_user_logged_out, only: :destroy
+  
+  def destroy
+    super_user_uuid = current_super_user.uuid
+    super
+    Log.create(scope: 'auth_logs', user_id: super_user_uuid, event_type: 'super_user_logged_out', result: 'Success')
+  end
 
   def after_sign_in_path_for(resource)
     super_user_root_path
@@ -10,10 +15,6 @@ class SuperUsers::SessionsController < Devise::SessionsController
   
   def super_user_logged_in
     Log.create(scope: 'auth_logs', user_id: resource.uuid, event_type: 'super_user_logged_in', result: 'Success')
-  end
-  
-  def super_user_logged_out
-    Log.create(scope: 'auth_logs', user_id: current_super_user.uuid, event_type: 'super_user_logged_out', result: 'Success')
   end
 
 end
