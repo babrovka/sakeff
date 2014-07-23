de6a3c0202ae361cb148cba6a5ee0b4065f07210
cbrwizard <cbrwizard@gmail.com>
2014-07-23 17:31:33 +0400

    Creates screenshots for all currently existing views

--- spec/acceptance/screenshots/screenshots.rb
+++ spec/acceptance/screenshots/screenshots.rb
@@ -8,49 +8,63 @@ feature "All pages are created correctly" do
   let(:role) { create(:role) }
   let(:user) { create(:user) }
 
+  # Contains info about pages which should have screenshots.
+  # Update it manually after creating new page
+  # @note path: route path, access: guest/super_user/user,
+  #       file_name: path to view file, including its name
   let(:routes) do
     [
-     { path: new_user_session_path, role: :guest, file_name: "user_login" },
-     { path: root_path, role: :guest, file_name: "root" },
-     { path: new_super_user_session_path, role: :guest, file_name: "super_user_login" },
-     { path: super_user_root_path, role: :super_user, file_name: "super_user_root" },
-     # { path: library_path, role: :super_user, file_name: "library" }
+     { path: new_user_session_path, access: :guest, file_name: "users_sessions_new" },
+     { path: root_path, access: :guest, file_name: "root" },
+     { path: new_super_user_session_path, access: :guest, file_name: "super_users_sessions_new" },
+     { path: users_root_path, access: :user, file_name: "users_dashboard_index" },
+
+     { path: super_user_root_path, access: :super_user, file_name: "super_users_dashboard_index" },
+
+     { path: super_user_organizations_path, access: :super_user, file_name: "super_users_organizations_index" },
+     { path: new_super_user_organization_path, access: :super_user, file_name: "super_users_organization_new" },
+     { path: edit_super_user_organization_path(organization), access: :super_user, file_name: "super_users_organization_edit" },
+
+     { path: super_user_users_path, access: :super_user, file_name: "super_users_users_index" },
+     { path: new_super_user_user_path, access: :super_user, file_name: "super_users_users_new" },
+     { path: edit_super_user_user_path(user), access: :super_user, file_name: "super_users_users_edit" },
+
+     { path: super_user_roles_path, access: :super_user, file_name: "super_users_roles_index" },
+
+     { path: super_user_permissions_path, access: :super_user, file_name: "super_users_permissions_index" },
+
+     { path: library_path, access: :super_user, file_name: "library" }
     ]
   end
 
-  context 'on guest pages', js: true do
-    it "successfully opens and creates screenshots for them" do
-      routes.select{|hash| hash[:role] == :guest}.each do |route|
-        if route[:model_id].present?
+  shared_examples "screenshottable" do |access|
+    it "successfully opens pages and creates screenshots" do
+      routes.select{|hash| hash[:access] == access}.each do |route|
+        visit route[:path]
+        screenshot_name = "#{route[:file_name]}.jpeg"
+        screenshot_path = "#{Rails.root.join("test_images", "screenshots")}/#{screenshot_name}"
+        page.save_screenshot(screenshot_path, full: true)
 
-        else
-          visit route[:path]
-          screenshot_name = "#{route[:file_name]}.jpeg"
-          screenshot_path = "#{Rails.root.join("test_images", "screenshots")}/#{screenshot_name}"
-          page.save_screenshot(screenshot_path, full: true)
-
-          expect(current_path).to eq route[:path]
-        end
+        expect(current_path).to eq route[:path]
       end
     end
   end
 
+  context 'on guest pages', js: true do
+    it_behaves_like "screenshottable", :guest
+  end
+
+  context 'on user pages', js: true do
+    before { login_as(user, scope: :user) }
+
+    it_behaves_like "screenshottable", :user
+  end
+
   context 'on super_user pages', js: true do
-    before { login_as(super_user, :scope => :super_user) }
-
-    it "successfully opens and creates screenshots for them" do
-      routes.select{|hash| hash[:role] == :super_user}.each do |route|
-        if route[:model_id].present?
-
-        else
-          visit route[:path]
-          screenshot_name = "#{route[:file_name]}.jpeg"
-          screenshot_path = "#{Rails.root.join("test_images", "screenshots")}/#{screenshot_name}"
-          page.save_screenshot(screenshot_path, full: true)
-
-          expect(current_path).to eq route[:path]
-        end
-      end
+    before do
+      login_as(super_user, scope: :super_user)
     end
+
+    it_behaves_like "screenshottable", :super_user
   end
 end
