b527755445ddf18d9a76d915f94c16d8ef5a62ba
cbrwizard <cbrwizard@gmail.com>
2014-07-23 18:09:44 +0400

    Added dev gems and first annotation for models

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: permissions
+#
+#  id          :uuid             not null, primary key
+#  title       :string(32)       not null
+#  description :string(255)      not null
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Permission < ActiveRecord::Base
   validates :title, :description, presence: true
   has_many :user_permissions
