eb07174c0fc2dba2992eb76982fe61efa923b13f
babrovka <babrovka@gmail.com>
2014-07-23 12:24:18 +0400

    syntax remarks

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,6 +1,6 @@
 class Log < ActiveRecord::Base
   validates :scope, :user_id, :event_type, :result, presence: true
-  validates :result, :inclusion => { :in => %w(Success Error)}
+  validates :result, inclusion: {in: %w(Success Error)}
   
   scope :auth_logs, -> { where(scope: 'auth_logs') }
   scope :user_logs, -> { where(scope: 'user_logs') }
