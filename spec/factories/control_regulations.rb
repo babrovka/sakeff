badf4fb1cb3d53751219909c520f11cebe56a5c6
Vladimir Krivchenko <vladimir.krivchenko@gmail.com>
2014-07-23 18:18:56 +0400

    Remove excess files.

--- spec/factories/control_regulations.rb
+++
@@ -1,7 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :control_regulation, :class => 'Control::Regulations' do
-    name "MyString"
-  end
-end
--- spec/factories/control_states.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :control_state, :class => 'Control::State' do
-    name "MyString"
-    system_name "MyString"
-    control_regulation_id 1
-  end
-end
