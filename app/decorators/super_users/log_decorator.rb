e3825216e326ebd59e348f9a324da0151fca5b48
tagirshine <tagirr1@yandex.ru>
2014-07-24 19:10:59 +0400

    styles and decorates ended

--- app/decorators/super_users/log_decorator.rb
+++ app/decorators/super_users/log_decorator.rb
@@ -10,10 +10,30 @@ class SuperUsers::LogDecorator < Draper::Decorator
     username
   end
 
+  def objectname
+    #object.obj_id.lol
+    if Organization.where(id: object.obj_id).present?
+      objectname = Organization.where(id: object.obj_id).first.short_title
+    elsif User.where(id: object.obj_id).present?
+      objectname = User.where(id: object.obj_id).first
+    else
+      objectname = object.obj_id
+    end
+
+    objectname
+  end
+
   def username_with_hint
     h.content_tag :span, username, title: "UUID: #{user.uuid}"
   end
+  def obj_id_with_hint
+    if object.obj_id?
+      h.content_tag :span, objectname
+      #, title: "UUID: #{obj_id_uuid}"
+    else
 
+    end
+  end
   def event_type_description
     I18n.t(object.event_type, scope: 'super_users.logs.index.table.results')
   end
@@ -40,6 +60,12 @@ class SuperUsers::LogDecorator < Draper::Decorator
     @user ||= SuperUser.where(uuid: object.user_id).first || User.where(id: object.user_id).first
     @user.uuid ||= @user.uuid || @user.id
     @user
+  end
+
+  def obj_id_uuid
+    @obj_id_uuid ||= User.where(id: object.obj_id).first || Organization.where(id: object.obj_id).first
+    #@obj_id_uuid.uuid ||= @obj_id_uuid.uuid || @obj_id_uuid.id
+    @obj_id_uuid
   end
 
 
