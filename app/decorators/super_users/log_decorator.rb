a66709a3c75104eef8eb2885b30479dabfb81eb4
tagirshine <tagirr1@yandex.ru>
2014-07-25 17:34:42 +0400

    fix pull requests

--- app/decorators/super_users/log_decorator.rb
+++ app/decorators/super_users/log_decorator.rb
@@ -11,16 +11,19 @@ class SuperUsers::LogDecorator < Draper::Decorator
   end
 
   def objectname
-    #object.obj_id.lol
-    if Organization.where(id: object.obj_id).present?
-      objectname = Organization.where(id: object.obj_id).first.short_title
-    elsif User.where(id: object.obj_id).present?
-      objectname = User.where(id: object.obj_id).first.username
+
+    subject = Organization.where(id: object.obj_id).first ||
+        User.where(id: object.obj_id).first ||
+        object.obj_id
+
+    if subject.is_a? Organization
+      subject.short_title
+    elsif subject.is_a? User
+      subject.username
     else
-      objectname = object.obj_id
+      subject
     end
 
-    objectname
   end
 
   def username_with_hint
@@ -28,8 +31,7 @@ class SuperUsers::LogDecorator < Draper::Decorator
   end
   def obj_id_with_hint
     if object.obj_id?
-      h.content_tag :span, objectname
-      #, title: "UUID: #{obj_id_uuid}"
+      h.content_tag :span, objectname, title: "UUID: #{object.obj_id}"
     else
 
     end
@@ -39,19 +41,19 @@ class SuperUsers::LogDecorator < Draper::Decorator
   end
 
   def comment
-    if object.comment?
-      object.comment
-    else
-      '-'
-    end
+    object.comment || '-'
   end
 
   def result
+    class_name = (object.result=='Success') ? 'text-green' : 'text-red'
+
     if object.result=='Success'
-      h.content_tag :span, object.result, class: 'text-green'
+      h.content_tag :span, object.result, class: class_name
     else
-      h.content_tag :span, object.result, class: 'text-red'
+      h.content_tag :span, object.result, class: class_name
     end
+
+
   end
 
   private
@@ -64,7 +66,6 @@ class SuperUsers::LogDecorator < Draper::Decorator
 
   def obj_id_uuid
     @obj_id_uuid ||= User.where(id: object.obj_id).first || Organization.where(id: object.obj_id).first
-    #@obj_id_uuid.uuid ||= @obj_id_uuid.uuid || @obj_id_uuid.id
     @obj_id_uuid
   end
 
