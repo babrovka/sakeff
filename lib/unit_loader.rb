5bcf34c6d23f38a98f8b95b59f24764f4c96c069
babrovka <babrovka@gmail.com>
2014-07-28 15:24:33 +0400

    advance unit loader

---
+++ lib/unit_loader.rb
@@ -0,0 +1,35 @@
+require 'spreadsheet'
+require 'fileutils'
+
+class UnitLoader
+  def initialize file_path, sheet_name
+    # Файл не существует?
+    raise "Can't find #{file_path}" unless File.exists? file_path
+
+    book = Spreadsheet.open file_path
+    @sheet = book.worksheet sheet_name
+
+    # Лист не существует?
+    raise "Sheet #{sheet_name} doesn't exists" unless @sheet
+  end
+
+  def load_units
+    @sheet.each_with_index do |row, index|
+      next if index == 0
+
+      Unit.create({
+        label: row[0],
+        id: row[1],
+        parent_id: row[2]
+      })
+      
+      Unit.all.each do |unit|
+        unit.has_children = unit.children.count
+        unit.save!
+      end
+
+    end
+    puts 'Units imported'
+  end
+
+end
