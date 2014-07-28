f000634588067b341aa7f2691cda175c98f952fe
justvitalius <justvitalius@gmail.com>
2014-07-28 17:45:34 +0400

    Fix incorrect clicking on library example links.

--- app/assets/javascripts/library.js
+++ app/assets/javascripts/library.js
@@ -1,3 +1,9 @@
 //
 //= require ./shared
 //= require_tree ./application
+//= require_self
+
+// убираем стандартные клики по ссылкам, которые перемещают вначало страницы
+$(document).ready(function(){
+    $('.lib-example a').on('click', function(e){e.preventDefault()});
+});
