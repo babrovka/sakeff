c47d37748ffd8cfa73b45dae15d8dd08a37e2d6a
babrovka <babrovka@gmail.com>
2014-07-28 14:12:19 +0400

    unit model

---
+++ app/models/unit.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: units
+#
+#  id           :uuid             not null, primary key
+#  label        :string(255)      not null
+#  parent_id    :uuid
+#  has_children :integer          default(0)
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
+class Unit < ActiveRecord::Base
+  acts_as_tree order: "label"
+end
