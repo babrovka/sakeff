471d4ad01b53e3a7f527afb884a8c919fb50192c
justvitalius <justvitalius@gmail.com>
2014-07-30 13:40:03 +0400

    Remove admin assets.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,5 +5,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( admin.js  super_admin.js  library.js )
-Rails.application.config.assets.precompile += %w( admin.css super_admin.css library.css )
+Rails.application.config.assets.precompile += %w( super_admin.js  library.js )
+Rails.application.config.assets.precompile += %w( super_admin.css library.css )
