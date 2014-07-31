3d55aced9b5369ce750dd9442306dafb6e6633cb
justvitalius <justvitalius@gmail.com>
2014-07-31 18:04:12 +0400

    Unknown fix.

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,18 +50,20 @@ set :hipchat_room_name, "430075"
 set :hipchat_announce, true
 
 
-namespace :deploy do
-  namespace :assets do
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile --trace}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
-    end
-  end
-end
+# закомментировать перед первым или чистым деплоем
+# иначе ошибочки будут
+# namespace :deploy do
+#   namespace :assets do
+#     task :precompile, :roles => :web, :except => { :no_release => true } do
+#       from = source.next_revision(current_revision)
+#       if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+#         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile --trace}
+#       else
+#         logger.info "Skipping asset pre-compilation because there were no asset changes"
+#       end
+#     end
+#   end
+# end
 
 
 namespace(:thin) do
