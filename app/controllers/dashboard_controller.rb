a5a6a3b09f6ec87f60dbd7981fd8015facd00172
justvitalius <justvitalius@gmail.com>
2014-08-05 19:15:02 +0400

    Add permission checking on open global-state-dashboard-page.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,7 +8,13 @@ class DashboardController < BaseController
   def global_state_dashboard
     permission_title = 'supervisor'
 
+    # тут по идее нужно через can? работать.
+    # это уже работа бэкенд специалистов
     @permission ||= Permission.where(title: permission_title).first
+
+    unless @permission && current_user.permissions.include?(@permission)
+      redirect_to users_root_path
+    end
   end
 
 
