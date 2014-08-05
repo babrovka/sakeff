0ab9de9f00b7d699fc65ee13c481d518d07dfd26
justvitalius <justvitalius@gmail.com>
2014-08-05 13:31:06 +0400

    Add unit importer.

--- lib/importers/unit_importer.rb
+++ lib/importers/unit_importer.rb
@@ -1,23 +1,24 @@
-class Importers::RoleImporter < Importers::Importer
+class Importers::UnitImporter < Importers::Importer
   class << self
 
     private
 
     def save_data(row, index)
-      unless index == 0 || Role.exists?(:title => row[0])
-        Role.create({
-                        title: row[0],
-                        description: row[1]
+      unless index == 0 || Unit.exists?(:label => row[0])
+        Unit.create({
+                        label: row[0],
+                        id: row[1],
+                        parent_id: row[2]
                     })
       end
     end
 
     def default_file_path
-      'db/excel/roles_and_permissions.xls'
+      'db/excel/units.xls'
     end
 
     def default_sheet_name
-      'roles'
+      'units'
     end
 
   end
