1c605d05f4e904895332b4770e7f2b627b637e29
justvitalius <justvitalius@gmail.com>
2014-08-06 12:49:11 +0400

    Add some control states to seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,8 @@
 # SuperUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', label: 'admin')
 Rake::Task['dev:org_and_user'].invoke
+Rake::Task['excel:permissions'].invoke
+Rake::Task['excel:units'].invoke
+
+Control::State.create(name: 'Пожар', system_name: 'fire')
+Control::State.create(name: 'Потом', system_name: 'cataclysm')
+Control::State.create(name: 'Ясно, можно не волновяться', system_name: 'so_good')
