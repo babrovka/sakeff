096d0de2f23a87ca31ee6f6159c32d7a887b338c
kirillnikitin <kitill22@mail.ru>
2014-08-06 16:32:19 +0400

    Modify links

--- config/navigations/user_navigation.rb
+++ config/navigations/user_navigation.rb
@@ -8,12 +8,12 @@ SimpleNavigation::Configuration.run do |navigation|
     navigation.name_generator = Proc.new { |name, item| "<span class='fa'></span>#{name}<span class='triangle'></span>" }
     navigation.consider_item_names_as_safe = true
 
-    primary.item :units, 'Объекты', users_root_path do |second_level|
+    primary.item :units, 'Объекты', units_path do |second_level|
       second_level.item :first, 'Первая ссылка', '#'
       second_level.item :second, 'Вторая ссылка', '#'
       second_level.item :third, 'Третья ссылка', '#'
     end
-    primary.item :unit1, 'Дспетчер', units_path do |second_level|
+    primary.item :unit1, 'Диспетчер', '#' do |second_level|
       second_level.item :first1, 'Первая ссылка', '#'
       second_level.item :second1, 'Вторая ссылка', '#'
       second_level.item :third1, 'Третья ссылка', '#'
@@ -22,14 +22,14 @@ SimpleNavigation::Configuration.run do |navigation|
       second_level.item :third1, 'Ссылка', '#'
       second_level.item :third1, 'Сылка', '#'
     end
-    primary.item :unit2, 'Сообщения', units_path
-    primary.item :settings, 'Настройки', units_path do |second_level|
+    primary.item :unit2, 'Сообщения', '#'
+    primary.item :settings, 'Настройки', '#' do |second_level|
       second_level.item :first3, 'Первая ссылка', '#'
       second_level.item :second3, 'Вторая ссылка', '#'
       second_level.item :third3, 'Третья ссылка', '#'
       second_level.item :third3, 'Ссылка', '#'
       second_level.item :third3, 'Ссылка', '#'
     end
-    primary.item :unit4, 'Объекты', units_path
+    primary.item :unit4, 'Объекты', '#'
   end
 end
