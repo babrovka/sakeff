575285ba89985cf78ecd08c8e05e149c172fe127
justvitalius <justvitalius@gmail.com>
2014-08-12 12:07:35 +0400

    Add private pub ssl patch.

---
+++ config/initializers/private_pub_ssl_patch.rb
@@ -0,0 +1,20 @@
+module PrivatePub
+
+  class << self
+    # Sends the given message hash to the Faye server using Net::HTTP.
+    def publish_message(message)
+      raise Error, "No server specified, ensure private_pub.yml was loaded properly." unless config[:server]
+      url = URI.parse(config[:server])
+
+      form = Net::HTTP::Post.new(url.path.empty? ? '/' : url.path)
+      form.set_form_data(:message => message.to_json)
+
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = url.scheme == "https"
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      http.start { |h| h.request(form) }
+    end
+
+  end
+
+end
