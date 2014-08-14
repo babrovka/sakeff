ec2c04f3353ace0d161c20afe3eea59923cfc705
cbrwizard <cbrwizard@gmail.com>
2014-08-14 19:31:03 +0400

    Normalized seeds

--- app/controllers/im/messages_controller.rb
+++ app/controllers/im/messages_controller.rb
@@ -3,29 +3,30 @@ class Im::MessagesController < BaseController
   before_action :authenticate_user!
   before_filter :authorize_dispather, only: [:new, :create]
 
+  # @note GET /messages
   def index
     @messages = current_user.inbox_messages
   end
 
+  # @note GET /messages/new
   def new
     @message = Im::Message.new
   end
 
+  # Creates message and sends it to recipients
+  # @note POST /messages
   def create
     @message = Im::Message.new(permitted_params)
 
     if @message.save
-      recipients =
-        if params[:im_message][:send_to_all] == "1"
-          User.all
-        else
-          User.where(id: params[:im_message][:recipient_ids].delete_if(&:blank?))
-        end
+      recipients = if params[:im_message][:send_to_all] == "1"
+        User.all
+      else
+        User.where(id: params[:im_message][:recipient_ids].delete_if(&:blank?))
+      end
       @message.recipients << recipients
 
-      recipients.each do |recipient|
-        PrivatePub.publish_to "/messages/#{recipient.id}", unread_messages_amount: 5
-      end
+      publish_messages_notification(recipients)
 
       redirect_to message_path(@message), notice: 'Message was successfully created.'
     else
@@ -33,11 +34,21 @@ class Im::MessagesController < BaseController
     end
   end
 
+  # @note GET /messages/:id
   def show
     @message = Im::Message.find(params[:id])
   end
 
   private
+
+  # Publishes to all related users that they have new unread messages
+  # @note is called on #create
+  # @param recipients [Array of Active Record User Collection]
+  def publish_messages_notification(recipients)
+    recipients.each do |recipient|
+      PrivatePub.publish_to "/broadcast/messages/#{recipient.id}", unread_messages_amount: 5
+    end
+  end
 
   def permitted_params
     params.require(:im_message).permit(:text, :sender_id).merge(sender_id: current_user.id)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,30 @@
-# SuperUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', label: 'admin')
+seed_models = [
+  {super_users: [{email: 'admin@example.com', password: 'password', password_confirmation: 'password', label: 'admin'}]},
+  {organizations: [{inn: "2212321223", short_title: "Org", full_title: "Organization title", legal_status: 1}]},
+  {users: [
+      {username: "admin", password: "password", password_confirmation: 'password', title: "not so admin", organization: Organization.first},
+      {username: "loller", password: "qwertyuiop", password_confirmation: 'qwertyuiop', title: "lolguy", organization: Organization.first}
+  ]}
+]
+
+seed_models.each do |model_hash|
+  hash_name = model_hash.keys[0].to_s
+  class_name = eval hash_name.camelize.singularize
+  seed_records = model_hash.values.flatten
+
+  if class_name.count < seed_records.size
+    class_name.create(seed_records)
+  end
+end
+
+
 # Rake::Task['dev:org_and_user'].invoke
 Rake::Task['excel:permissions'].invoke
 Rake::Task['excel:units'].invoke
-
-
 Rake::Task['dev:import_states'].invoke
 
+# Convert first user to diSPATCHARR
+user = User.first
+permission = Permission.where(title: "dispatcher")
+user.permissions << permission
+UserPermission.where(user: user, permission: permission).first.update(result: 1)
