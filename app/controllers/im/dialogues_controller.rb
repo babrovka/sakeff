472be43826e59f3e5be07b8946c6bb19e70a32df
justvitalius <justvitalius@gmail.com>
2014-08-20 16:08:57 +0400

    Render dialogue messages list.

--- app/controllers/im/dialogues_controller.rb
+++ app/controllers/im/dialogues_controller.rb
@@ -1,6 +1,11 @@
 class Im::DialoguesController < BaseController
 
-  helper_method :collection, :d_collection
+  helper_method :collection,
+                :d_collection,
+                :resource,
+                :d_resource,
+                :messages,
+                :d_messages
 
   def index
   end
@@ -16,6 +21,22 @@ class Im::DialoguesController < BaseController
 
   def d_collection
     @d_dialogues ||= Im::DialoguesDecorator.decorate(collection)
+  end
+
+  def resource
+    @dialogue ||= Im::Dialogue.where(id: params[:id]).includes(:messages).first
+  end
+
+  def d_resource
+    @d_dialogue ||= Im::DialogueDecorator.decorate resource
+  end
+
+  def messages
+    @messages ||= resource.messages.order('created_at ASC')
+  end
+
+  def d_messages
+    @d_messages ||= Im::MessagesDecorator.decorate messages
   end
 
 
