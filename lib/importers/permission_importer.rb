31a9fcbd69114b93b8530943bb1dc4d1b8ed5b81
babrovka <babrovka@gmail.com>
2014-08-26 15:49:55 +0400

    new permissions uploader

--- lib/importers/permission_importer.rb
+++ lib/importers/permission_importer.rb
@@ -2,22 +2,28 @@ class Importers::PermissionImporter < Importers::Importer
   class << self
 
     private
+    
+    def before_import
+      Permission.destroy_all
+      puts 'permissions destroyed'
+    end
 
     def save_data(row, index)
-      unless index == 0 || Permission.exists?(:title => row[0])
+      unless index == 0 || Permission.exists?(:title => row[1])
         Permission.create({
-                              title: row[0],
-                              description: row[1]
+                              id: row[0],
+                              title: row[1],
+                              description: row[2]
                           })
       end
     end
 
     def default_file_path
-      'db/excel/roles_and_permissions.xls'
+      'db/excel/Permissions.xls'
     end
 
     def default_sheet_name
-      'permissions'
+      'Permissions'
     end
 
   end
