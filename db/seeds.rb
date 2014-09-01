5878b53caa0a91d4de057da8f76acaf48d3f605c
Vladimir Krivchenko <vladimir.krivchenko@gmail.com>
2014-09-01 11:49:43 +0400

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,4 @@
-seed_models = [
-  {super_users: [{email: 'admin@example.com', password: 'password', password_confirmation: 'password', label: 'admin'}]},
-  {organizations: [{inn: "2212321223", short_title: "Org", full_title: "Organization title", legal_status: 1}]},
-  {users: [
-      {first_name: "Vasia", last_name: "Ivanov", username: "admin", password: "password", password_confirmation: 'password', title: "not so admin", organization: Organization.first},
-      {first_name: "Ulia", last_name: "Pupkina", middle_name: "MegaPuker", username: "loller", password: "qwertyuiop", password_confirmation: 'qwertyuiop', title: "lolguy", organization: Organization.first}
-  ]}
-]
-
-seed_models.each do |model_hash|
+def process_hash model_hash
   hash_name = model_hash.keys[0].to_s
   class_name = eval hash_name.camelize.singularize
   seed_records = model_hash.values.flatten
@@ -23,14 +14,14 @@ seed_models.each do |model_hash|
   end
 end
 
+process_hash({super_users: [{email: 'admin@example.com', password: 'password', password_confirmation: 'password', label: 'admin'}]})
+process_hash({organizations: [{inn: "2212321223", short_title: "Org", full_title: "Organization title", legal_status: 1}]})
+process_hash({users: [
+      {first_name: "Vasia", last_name: "Ivanov", middle_name: "TestTest", username: "admin", password: "password", password_confirmation: 'password', title: "not so admin", organization: Organization.first},
+      {first_name: "Ulia", last_name: "Pupkina", middle_name: "MegaPuker", username: "loller", password: "qwertyuiop", password_confirmation: 'qwertyuiop', title: "lolguy", organization: Organization.first}
+  ]})
 
 # Rake::Task['dev:org_and_user'].invoke
 Rake::Task['excel:permissions'].invoke
 Rake::Task['excel:units'].invoke
 Rake::Task['dev:import_states'].invoke
-
-# Convert first user to diSPATCHARR
-user = User.first
-permission = Permission.where(title: "dispatcher")
-user.permissions << permission
-UserPermission.where(user: user, permission: permission).first.update(result: 1)
