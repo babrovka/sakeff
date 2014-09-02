d65922c2459b0098c2445830e346a5845d8a8a1b
justvitalius <justvitalius@gmail.com>
2014-09-02 18:43:21 +0400

    Add mock for message test.

--- spec/acceptance/users/messages/manage_broadcast_messages_spec.rb
+++ spec/acceptance/users/messages/manage_broadcast_messages_spec.rb
@@ -1,22 +1,14 @@
 require 'acceptance_helper'
 require 'support/behaviours/units_tree_viewable'
 
-feature "User interacts with units", js: true, units: true do
+feature "User manage broadcast messages", js: true do
 
-  let(:user) { create(:user) }
+  let(:messages) { create(:user) }
   let!(:unit) { create(:unit) }
   let!(:child_unit) { create(:child_unit) }
   let!(:grandchild_unit) { create(:grandchild_unit) }
 
-  describe 'on index page' do
-    before do
-      login_as(user, :scope => :user)
-      visit units_path
-    end
 
-    pending("Because phantomjs doesn't support webgl") do
-      it_behaves_like :units_tree_viewable
-    end
+  describe ''
 
-  end
 end
