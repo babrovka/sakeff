cb2c588f9f5c125f172a97fb15fd7438a60d4b9e
kirillnikitin <kitill22@mail.ru>
2014-09-05 16:29:00 +0400

    Fix bugs, left menu improvement

--- config/navigations/user_navigation.rb
+++ config/navigations/user_navigation.rb
@@ -7,6 +7,9 @@ SimpleNavigation::Configuration.run do |navigation|
     navigation.active_leaf_class = 'active'
     navigation.consider_item_names_as_safe = true
 
+    primary.item :root, 'САКЭ КЗС', nil, class: 'brand_logo'
+
+    
     #primary.item :contoller, 'Дашбоард', users_root_path
 
     primary.item :dispatcher, 'Диспетчер', control_dashboard_path, notification_color: lambda { 'badge-green' }, if: proc { current_user.has_permission?(:access_dispatcher) }
