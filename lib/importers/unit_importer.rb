60da6dcf4d8572da035aa16aa2a999bf3b1ba785
babrovka <babrovka@gmail.com>
2014-09-09 12:54:30 +0400

    fix units importer

--- lib/importers/unit_importer.rb
+++ lib/importers/unit_importer.rb
@@ -4,28 +4,24 @@ class Importers::UnitImporter < Importers::Importer
     private
     
     def before_import
-      Unit.destroy_all
+      Unit.delete_all
       puts 'units destroyed'
     end
 
     def save_data(row, index)
-      unless index == 0 || Unit.exists?(:id => row[1]) || row[0] == nil
-        if row[2] == 0
-          row[2] = nil
-        end
-        unit = Unit.create({
-                        label: row[0],
-                        id: row[1],
-                        parent_id: row[2], 
-                        model_filename: row[3]
-                    })
-                    
-        puts "#{unit.id} | #{unit.parent_id}"
-        # puts "\"#{row[0]}\""
-        # puts "\"#{row[1]}\""
-        # puts "\"#{row[2]}\""
-        # puts "\"#{row[3]}\""
+      return if index == 0
+      return if !row[0].present?
+      return if !row[1].present?
+      puts "'#{index}' '#{row[0]}' '#{row[1]}' '#{!row[0].present?}'"
+      if row[2] == 0
+        row[2] = nil
       end
+      unit = Unit.create({
+                      label: row[0],
+                      id: row[1],
+                      parent_id: row[2], 
+                      model_filename: row[3]
+                  })
     end
 
     def after_import
