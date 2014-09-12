e9749d30a141891d0ed603d1aacb82b3bd445370
babrovka <babrovka@gmail.com>
2014-09-12 15:53:43 +0400

    deploy to demo

--- config/recipes/demo.rb
+++ config/recipes/demo.rb
@@ -22,11 +22,11 @@ task :demo do
   end
 
   task :import_permissions do
-     run %Q{cd #{latest_release} && RAILS_ENV=dev bundle exec rake excel:permissions}
+     run %Q{cd #{latest_release} && RAILS_ENV=demo bundle exec rake excel:permissions}
   end
 
   task :copy_and_import_units do
-     run %Q{cd #{latest_release} && cp source3d/objectTree.xls db/excel/units.xls && RAILS_ENV=dev bundle exec rake excel:units}
+     run %Q{cd #{latest_release} && cp source3d/objectTree.xls db/excel/units.xls && RAILS_ENV=demo bundle exec rake excel:units}
   end
 
   task :symlink_maps do
@@ -59,7 +59,7 @@ task :demo do
   set :scm, "git"
   set :repository, "git@github.com:babrovka/sakeff.git"
   set :branch, "dev"
-  set :rails_env, "dev"
+  set :rails_env, "demo"
 
   default_run_options[:pty] = true
   ssh_options[:forward_agent] = true
@@ -86,11 +86,11 @@ task :demo do
 
   namespace(:thin) do
     task :stop do
-      run %Q{cd #{latest_release} && RAILS_ENV=dev bundle exec thin stop -C #{shared_path}/sakedev.yml}
+      run %Q{cd #{latest_release} && RAILS_ENV=demo bundle exec thin stop -C #{shared_path}/sakedev.yml}
      end
 
     task :start do
-      run %Q{cd #{latest_release} && RAILS_ENV=dev bundle exec thin start -C #{shared_path}/sakedev.yml}
+      run %Q{cd #{latest_release} && RAILS_ENV=demo bundle exec thin start -C #{shared_path}/sakedev.yml}
     end
 
     task :restart do
@@ -104,8 +104,8 @@ task :demo do
     desc "Start private_pub server"
     task :start do
       # old command
-      run %Q{cd #{latest_release} && RAILS_ENV=dev bundle exec thin start -C #{shared_path}/private_pub.yml }
-      # run %Q{cd #{latest_release} && RAILS_ENV=dev bundle exec thin -C #{shared_path}/private_pub.yml start }
+      run %Q{cd #{latest_release} && RAILS_ENV=demo bundle exec thin start -C #{shared_path}/private_pub.yml }
+      # run %Q{cd #{latest_release} && RAILS_ENV=demo bundle exec thin -C #{shared_path}/private_pub.yml start }
 
     end
 
@@ -114,7 +114,7 @@ task :demo do
     task :stop do
       # old command
       # run "cd #{current_path};if [ -f tmp/pids/private_pub.pid ] && [ -e /proc/$(cat tmp/pids/private_pub.pid) ]; then kill -9 `cat tmp/pids/private_pub.pid`; fi"
-      run %Q{cd #{latest_release} && RAILS_ENV=dev bundle exec thin -C #{shared_path}/private_pub.yml stop}
+      run %Q{cd #{latest_release} && RAILS_ENV=demo bundle exec thin -C #{shared_path}/private_pub.yml stop}
     end
 
     desc "Restart private_pub server"
@@ -126,7 +126,7 @@ task :demo do
 
   namespace(:populate) do
     task :data do
-      run %Q{cd #{latest_release} && bundle exec rake db:seed RAILS_ENV=dev}
+      run %Q{cd #{latest_release} && bundle exec rake db:seed RAILS_ENV=demo}
     end
   end
 
