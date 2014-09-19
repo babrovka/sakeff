5d59bc870c5ad9c30402589dd39e37f9524f67e4
Vladimir Krivchenko <vladimir.krivchenko@gmail.com>
2014-09-19 16:35:59 +0400

    Remove recipients from message factory

--- spec/factories/im/message.rb
+++ spec/factories/im/message.rb
@@ -1,11 +1,6 @@
 FactoryGirl.define do
-
   factory :message, class: Im::Message do
-
     text { "Message text ##{generate(:number)}"}
-    recipients { [create(:user)] }
     sender { create(:user) }
-
   end
-
 end
