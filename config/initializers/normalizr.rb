c2ae2ce46011f4544b002067875007de234a44af
babrovka <babrovka@gmail.com>
2014-09-22 17:23:54 +0400

    refactor normalizr.rb

--- config/initializers/normalizr.rb
+++ config/initializers/normalizr.rb
@@ -2,12 +2,6 @@ Normalizr.configure do
   default :strip, :blank
   
   add :cell_phone do |value|
-    if String === value
-      value = value.gsub(/[^0-9]+/, '')
-      value = value.to_s[0..10]
-      value unless value.empty?
-    else
-      value
-    end
+    value.instance_of?(String) ? value.gsub(/[^0-9]+/, '').to_s[0..10] : value
   end
 end
