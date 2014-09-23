658f059643e264c0619b7bc2b91ed912324308fe
babrovka <babrovka@gmail.com>
2014-09-23 16:05:34 +0400

    dialogue has sender and reciever upon initialization

--- spec/models/im/dialogue.rb
+++ spec/models/im/dialogue.rb
@@ -5,24 +5,30 @@ describe Im::Dialogue do
   describe '#messages' do
     context 'between organization' do
       
-      let!(:dialogue) { Im::Dialogue.new(:organization) }
+      
       let!(:sender_organization) { create(:organization) }
       let!(:recipient_organization) { create(:organization) }
       let!(:third_organization) { create(:organization) }
+      let!(:dialogue) { Im::Dialogue.new(:organization, sender_organization.id, recipient_organization.id) }
       
       before do
-        expect(dialogue.messages(sender_organization.id, recipient_organization.id).count).to eq 0
+        expect(dialogue.messages.count).to eq 0
         Im::Message.create!(sender_id: sender_organization.id, receiver_id: recipient_organization.id, text: 'hello', reach: :organization)
         Im::Message.create!(sender_id: recipient_organization.id, receiver_id: sender_organization.id, text: 'reply', reach: :organization)
       end
-  
+      
+      it 'has sender and receiver' do 
+        expect(dialogue.sender_id).to eq sender_organization.id
+        expect(dialogue.reciever_id).to eq recipient_organization.id
+      end
       
       it "when messages method set up correct" do
-        expect(dialogue.messages(sender_organization.id, recipient_organization.id).count).to eq 2
+        expect(dialogue.messages.count).to eq 2
       end
       
       it "when messages method set up incorrect" do
-        expect(dialogue.messages(sender_organization.id, third_organization.id).count).to eq 0
+        dialogue = Im::Dialogue.new(:organization, sender_organization.id, third_organization.id)
+        expect(dialogue.messages.count).to eq 0
       end
     
     end
