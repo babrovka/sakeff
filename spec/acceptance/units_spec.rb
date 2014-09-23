ea7c73f82e707017ac83f037ddc8772a29c7a971
cbrwizard <cbrwizard@gmail.com>
2014-09-23 18:35:52 +0400

    Adds units structure tests

--- spec/acceptance/units_spec.rb
+++ spec/acceptance/units_spec.rb
@@ -17,6 +17,10 @@ feature "User interacts with units", js: true, units: true do
   let!(:grandchild_unit) { create(:grandchild_unit, parent: child_unit) }
 
   describe 'on index page' do
+    def open_bubble
+      first(".js-bubble-open").click
+    end
+
     before do
       login_as(user, :scope => :user)
       visit units_path
@@ -27,22 +31,6 @@ feature "User interacts with units", js: true, units: true do
     context 'has no units manipulation rights' do
       it "doesn't display bubbles add button" do
         expect(page).to_not have_css ".m-tree-add"
-      end
-    end
-
-    context 'has got units manipulation rights' do
-
-      def open_bubble
-        first(".js-bubble-open").click
-      end
-
-      before do
-        login_as(allowed_user, :scope => :user)
-        visit units_path
-      end
-
-      it "displays bubbles add button" do
-        expect(page).to have_css ".m-tree-add"
       end
 
       it "has got one bubble at first" do
@@ -56,30 +44,45 @@ feature "User interacts with units", js: true, units: true do
           expect(page).to have_css('.js-bubble-text', text: unit_bubble.comment)
         end
       end
+    end
 
-      it "creates new bubble" do
-        bubbles_amount_before = UnitBubble.count
-        first(".m-tree-add").click
-        within(".m-active") do
-          find("select").find(:xpath, 'option[2]').select_option
-          fill_in 'unit_bubble[comment]', with: 'Bubble comment'
-          click_button("Сохранить")
-        end
-        sleep(5)
+    context 'has got units manipulation rights' do
 
-        expect(UnitBubble.count).to eq bubbles_amount_before + 1
+      before do
+        login_as(allowed_user, :scope => :user)
+        visit units_path
       end
 
-      it "deletes a bubble" do
-        bubbles_amount_before = UnitBubble.count
-        open_bubble
-        within(".m-active") do
-          click_link("Удалить")
-        end
-        sleep(5)
+      it "displays bubbles add button" do
+        expect(page).to have_css ".m-tree-add"
+      end
 
-        expect(UnitBubble.count).to eq bubbles_amount_before - 1
+      describe "manipulates with bubbles" do
+        it "creates new bubble" do
+          bubbles_amount_before = UnitBubble.count
+          first(".m-tree-add").click
+          within(".m-active") do
+            find("select").find(:xpath, 'option[2]').select_option
+            fill_in 'unit_bubble[comment]', with: 'Bubble comment'
+            click_button("Сохранить")
+          end
+          sleep(5)
+
+          expect(UnitBubble.count).to eq bubbles_amount_before + 1
+        end
+
+        it "deletes a bubble" do
+          bubbles_amount_before = UnitBubble.count
+          open_bubble
+          within(".m-active") do
+            click_link("Удалить")
+          end
+          sleep(5)
+
+          expect(UnitBubble.count).to eq bubbles_amount_before - 1
+        end
       end
     end
+
   end
 end
--- spec/support/behaviours/units_tree_viewable.rb
+++ spec/support/behaviours/units_tree_viewable.rb
@@ -1,14 +1,38 @@
 # Tests whenever users can interact with units via a jstree
 shared_examples :units_tree_viewable do
-  describe 'at start' do
+  before(:each) do
+    sleep(3)
+  end
+
+  describe 'at start', js: true do
     it 'already has several opened nodes at start' do
-      expect(page.all(".jstree-node").count).to be > 1
+      expect(page.all(".js-units-tree-container .jstree-node").count).to be > 1
+    end
+
+    it "displays units in a correct structure" do
+      all('.jstree-ocl').last.click
+      sleep(3)
+
+      node = page.find '.jstree-anchor', text: unit.label
+      container = node.find(:xpath, '..')
+      within(container) do
+        expect(page).to have_text child_unit.label
+      end
+
+      child_node = page.find '.jstree-anchor', text: grandchild_unit.label
+      child_container = child_node.find(:xpath, '..')
+
+      within(child_container) do
+        expect(page).to have_text grandchild_unit.label
+      end
     end
   end
 
-  describe 'clicking on them' do
+  describe 'clicking on them', js: true do
     it 'hides a node' do
       first(".jstree-ocl").click
+      sleep(3)
+
       expect(page.all(".jstree-ocl").count).to eq(1)
     end
 
