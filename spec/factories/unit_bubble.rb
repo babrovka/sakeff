543abcfee714a3a2ec6d5dfaec7f421f5203578b
cbrwizard <cbrwizard@gmail.com>
2014-09-23 16:23:28 +0400

    Bubble contents test

--- spec/factories/unit_bubble.rb
+++ spec/factories/unit_bubble.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :unit_bubble do
     bubble_type { rand(4) }
-    unit
     comment { Faker::Lorem.sentence }
   end
 end
--- spec/support/behaviours/units_tree_viewable.rb
+++ spec/support/behaviours/units_tree_viewable.rb
@@ -1,26 +1,23 @@
 # Tests whenever users can interact with units via a jstree
 shared_examples :units_tree_viewable do
   describe 'at start' do
-    it 'has several nodes at start' do
+    it 'already has several opened nodes at start' do
       expect(page.all(".jstree-node").count).to be > 1
     end
   end
 
   describe 'clicking on them' do
-    before(:each) do
-      visit current_path
+    it 'hides a node' do
+      first(".jstree-ocl").click
+      expect(page.all(".jstree-ocl").count).to eq(1)
     end
 
     it 'opens a node' do
+      visit current_path
+
+      first(".jstree-ocl").click
       first(".jstree-ocl").click
       expect(page.all(".jstree-node").count).to be > 1
-    end
-
-    it 'hides a node' do
-      first(".jstree-ocl").click
-      sleep(5)
-      first(".jstree-ocl").click
-      expect(page.all(".jstree-ocl").count).to eq(1)
     end
   end
 end
