233726cfc9e8ae278a5ddbddc61485656945a7b6
Vladimir Krivchenko <vladimir.krivchenko@gmail.com>
2014-09-24 18:34:20 +0400

    Add specs for notifying (after broadcast/organization) and broadcast message sending.

--- spec/models/im/dialogue.rb
+++ spec/models/im/dialogue.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
-describe Im::Dialogue do
-  
+describe Im::Dialogue do 
   describe '#messages' do
+    context 'broadcast' do
+      let(:dialogue) {Im::Dialogue.new(:broadcast)}
+
+      it 'gets all broadcast messages' do
+        3.times {create(:message)}
+        3.times {create(:organization_message)}
+
+        expect(dialogue.messages.count).to be == Im::Message.broadcast.count
+
+        dialogue.messages.each {|m| expect(m.reach).to be == 'broadcast' }
+      end
+    end
+
     context 'between organization' do
-      
-      
+
       let!(:sender_organization) { create(:organization) }
       let!(:recipient_organization) { create(:organization) }
       let!(:third_organization) { create(:organization) }
@@ -35,25 +46,33 @@ describe Im::Dialogue do
   end
 
   describe '#send' do
+    context 'broadcast' do
+      let!(:sender_user) { create(:user) }
+      let!(:dialogue) {Im::Dialogue.new(:broadcast)}
+
+      it 'sends message' do
+        message = Im::Message.new(sender_user_id: sender_user.id, text: 'text')
+
+        expect(message).to receive(:notify_interesants)
+        expect {dialogue.send(message)}.to change {Im::Message.count}.by(1)
+        expect(message.reach).to be == 'broadcast'
+      end
+    end
+
     context 'between organization' do
-      
       let!(:dialogue) { Im::Dialogue.new(:organization) }
       let!(:sender_organization) { create(:organization) }
       let!(:sender_user) { create(:user, organization: sender_organization) }
       let!(:recipient_organization) { create(:organization) }
-  
       
       it "sends message" do
         message = Im::Message.new(sender_user_id: sender_user.id, receiver_id: recipient_organization.id, text: 'text')
+        expect(message).to receive(:notify_interesants)
         dialogue.send(message)
         expect(message.reach).to eq 'organization'
         expect(message.sender_id).to eq sender_organization.id
         expect(message.receiver_id).to eq recipient_organization.id
       end
-
-    
     end
   end
-
-
 end
