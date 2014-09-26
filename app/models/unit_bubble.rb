b47ca9854b4abdbf50b30895c085a8e4f9e1473d
babrovka <babrovka@gmail.com>
2014-09-26 16:37:53 +0400

    grouped_bubbles_for_all_units

--- app/models/unit_bubble.rb
+++ app/models/unit_bubble.rb
@@ -15,40 +15,30 @@ class UnitBubble < ActiveRecord::Base
   enum bubble_type: [ :facilities_accident, :work, :information, :emergency ]
   belongs_to :unit
   
-  def self.grouped_bubbles
+  def self.grouped_bubbles_for_all_units
+    self.sql.group_by do |x|
+      x['id']
+    end.map do |k, v|
+      b = {}
+      v.each do |t|
+        type_name = UnitBubble.bubble_types.key(t['bubble_type'].to_i)
+        b[t['bubble_type']] = {
+          name: type_name,
+          russian_name: I18n.t(type_name, scope: "enums.unit_bubble.bubble_type."),
+          count: t['count']
+        }
+      end
+      {unit_id: k, bubbles: b}
+    end
+  end
+  
+  def self.sql
     sql_query = (<<-SQL)
     select units.id, bubbles.bubble_type, count(bubbles) from units 
     join unit_bubbles bubbles on bubbles.unit_id = units.id 
     group by units.id, bubbles.bubble_type
       SQL
-      
     ActiveRecord::Base.connection.execute(sql_query).to_a
-    
-    
   end
 
-  # @todo babrovka write comments
-  def self.bubbles_by_type_of_unit(unit)
-    UnitBubble.joins(:unit).where("units.lft >= ? AND units.rgt < ? OR units.id = ?", unit.left, unit.right, unit.id).group_by(&:bubble_type)
-  end
-
-  def self.grouped_bubbles_for_all_units
-    Unit.all.map do |unit|
-      self.count_bubbles_by_type(unit)
-    end.compact.select { |h| h.present? }
-  end
-
-  def self.count_bubbles_by_type(unit)
-    h = {}
-    self.bubbles_by_type_of_unit(unit).each do |bubble_type, bubbles|
-      h[UnitBubble.bubble_types[bubble_type]] = {name: bubble_type, russian_name: I18n.t("enums.unit_bubble.bubble_type.#{bubble_type}"), count: bubbles.count} unless bubbles.empty?
-    end
-    unless h.blank?
-      p = {}
-      p[:unit_id] = unit.id.upcase
-      p[:bubbles] = h
-
-      p.delete_if { |key, value| value.blank? }
-    end
-  end
 end
