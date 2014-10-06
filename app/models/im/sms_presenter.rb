65393e5a0f70b43d3aef22f8bde583ed966587db
Vladimir Krivchenko <vladimir.krivchenko@gmail.com>
2014-10-06 19:57:38 +0400

    Sms presenter adaptation.

--- app/models/im/sms_presenter.rb
+++ app/models/im/sms_presenter.rb
@@ -12,13 +12,16 @@ class Im::SmsPresenter
   end
     
   def self.send_message(number, message)
-    uri = URI("https://cabinet.kompeito.ru/api/plain/send?login=pdv@format-c.pro&pass=kOWFLU26te6mgfGV4RRQ&from=79204148114&to=#{number}&message=#{message}")
+    raise 'config/sms.yml configuration file is missing' unless File.exist?('config/sms.yml')
+    sms_config = YAML.load_file('config/sms.yml').symbolize_keys!
+
+    uri = URI("https://gate.smsaero.ru/send/?to=#{number}&text=#{message}&user=#{sms_config[:user]}&password=#{sms_config[:password]}&from=SAKE")
     request = Net::HTTP::Get.new uri.request_uri
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     res = http.start { |h| h.request(request) }
-    SMS_LOGGER.info("#{res.code} #{uri}")
+    SMS_LOGGER.info("#{res.code} #{res.body}")
   end
 end
 
