201a2f05eee290c8e6eb74841128d4d4523cc90d
justvitalius <justvitalius@gmail.com>
2014-10-14 15:45:51 +0400

    Try to fix error with private_pub notification engine.

--- app/models/notification_engine/private_pub.rb
+++ app/models/notification_engine/private_pub.rb
@@ -3,9 +3,9 @@ module NotificationEngine
   class PrivatePub
     def self.notify(user, message, _options={})
       channel = "/messages/private/#{user.id}"
-      # Rails.logger.info '---------------'
-      # Rails.logger.info "send notification about message through Websocket to #{channel}"
-      # Rails.logger.info '---------------'
+      Rails.logger.info '---------------'
+      Rails.logger.info "send notification about message through Websocket to #{channel}"
+      Rails.logger.info '---------------'
       ::PrivatePub.publish_to channel, { notification: { name: message.receiver_name, diff: 1, module: 'messages' }, message: message }
     end
   end
