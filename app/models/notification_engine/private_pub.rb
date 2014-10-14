406ccc211005495b0b39f009dc8294aa9b98efa1
justvitalius <justvitalius@gmail.com>
2014-10-14 16:07:46 +0400

    Try to fix error in private_pub notification engine.

--- app/models/notification_engine/private_pub.rb
+++ app/models/notification_engine/private_pub.rb
@@ -6,7 +6,7 @@ module NotificationEngine
       Rails.logger.info '---------------'
       Rails.logger.info "send notification about message through Websocket to #{channel}"
       Rails.logger.info '---------------'
-      ::PrivatePub.publish_to channel, { notification: { name: message.receiver_name, diff: 1, module: 'messages' }, message: message }
+      ::PrivatePub.publish_to channel, { notification: { name: "#{message.receiver_name}", diff: 1, module: 'messages' }, message: message.force_encoding('UTF-8') }
     end
   end
 end
