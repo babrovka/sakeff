32fc4ea5ca430b54c704e1f93799d5fa3f5aba81
justvitalius <justvitalius@gmail.com>
2014-10-14 15:20:03 +0400

    Fix error with encoding messages.

--- app/models/notification_engine/private_pub.rb
+++ app/models/notification_engine/private_pub.rb
@@ -1,11 +1,11 @@
 module NotificationEngine
   class PrivatePub
-    def self.notify user, message, options
+    def self.notify(user, message, _options={})
       channel = "/messages/private/#{user.id}"
-      Rails.logger.info '---------------'
-      Rails.logger.info "send notification about message through Websocket to #{channel}"
-      Rails.logger.info '---------------'
-      ::PrivatePub.publish_to channel, {notification: {name: message.receiver_name, diff: 1, module: 'messages'}, message: message}
+      # Rails.logger.info '---------------'
+      # Rails.logger.info "send notification about message through Websocket to #{channel}"
+      # Rails.logger.info '---------------'
+      ::PrivatePub.publish_to channel, { notification: { name: message.receiver_name, diff: 1, module: 'messages' }, message: message }
     end
   end
 end
