a72ab28289f100f00256614eadc470cf7753715c
babrovka <babrovka@gmail.com>
2014-10-22 16:17:54 +0400

    permit tests

--- spec/factories/permit.rb
+++ spec/factories/permit.rb
@@ -1,8 +1,26 @@
 FactoryGirl.define do
 
-  factory :permit do
+  factory :once_permit, class: Permit do
+    t = Time.now
+    starts_at t
+    expires_at t
+    person 'test'
+    location 'test'
   end
-
-
-
+  
+  factory :car_permit, class: Permit do
+    vehicle_type 'passenger'
+    car_brand 'volvo'
+    car_number 'K532CB'
+    region '178'
+  end
+  
+  factory :human_permit, class: Permit do
+    first_name 'first_name'
+    last_name 'last_name'
+    middle_name 'middle_name'
+    doc_type 'passport'
+    doc_number '12345678'
+  end
+  
 end
--- spec/models/permit_spec.rb
+++ spec/models/permit_spec.rb
@@ -1,103 +1,87 @@
 require 'rails_helper'
 
 describe Permit do
-  let(:permit) { create(:permit) }
+  let(:once_permit) { create(:once_permit) }
+  let(:car_permit) { create(:car_permit) }
+  let(:human_permit) { create(:human_permit) }
+  
 
   describe "available print types" do
 
     describe '#once? permit' do
-      before do
-        t = Time.now
-        permit.starts_at = t
-        permit.expires_at = t
-        permit.person = 'test'
-        permit.location = 'test'
-      end
       
       it "is possible when start and expire dates are the same" do
-        permit.save!
-        permit.reload
-        expect(permit.once?).to be_truthy
+        once_permit.save!
+        once_permit.reload
+        expect(once_permit.once?).to be_truthy
       end
       
       it "is impossible when start and expire dates are different" do
-        permit.expires_at = (Time.now - 2.days)
-        permit.save!
-        permit.reload
-        expect(permit.once?).to be_falsey
+        once_permit.expires_at = (Time.now + 2.days)
+        once_permit.save!
+        once_permit.reload
+        expect(once_permit.once?).to be_falsey
       end
       
       it "is impossible without location" do
-        permit.location = nil
-        permit.save!
-        permit.reload
-        expect(permit.once?).to be_falsey
+        once_permit.location = nil
+        once_permit.save!
+        once_permit.reload
+        expect(once_permit.once?).to be_falsey
       end
       
       it "is impossible without person" do
-        permit.person = nil
-        permit.save!
-        permit.reload
-        expect(permit.once?).to be_falsey
+        once_permit.person = nil
+        once_permit.save!
+        once_permit.reload
+        expect(once_permit.once?).to be_falsey
       end 
     end
     
-    describe '#car? permit' do
-      before do
-        permit.vehicle_type = 'passenger'
-        permit.car_brand = 'volvo'
-        permit.car_number = 'K532CB'
-        permit.region = '178'
-      end
-      
+    describe '#car? permit' do      
       it "is possible when car related fields are fulfilled" do
-        permit.save!
-        permit.reload
-        expect(permit.car?).to be_truthy
+        car_permit.save!
+        car_permit.reload
+        expect(car_permit.car?).to be_truthy
       end
       
       it "is impossible when one of required fields is empty" do
         %w[vehicle_type car_brand car_number region].each do |field|
-          permit.update_attribute(field, nil)
-          permit.save!
-          permit.reload
-          expect(permit.car?).to be_falsey
+          car_permit = FactoryGirl.create(:car_permit)
+          car_permit.update_attribute(field, nil)
+          car_permit.save!
+          car_permit.reload
+          expect(car_permit.car?).to be_falsey
         end
       end
     end
     
     describe '#human? permit' do
-      before do
-        permit.first_name = 'first_name'
-        permit.last_name = 'last_name'
-        permit.middle_name = 'middle_name'
-        permit.doc_type = 'passport'
-        permit.doc_number = '12345678'
-      end
       
       it "is possible when human related fields are fulfilled" do
-        permit.save!
-        permit.reload
-        expect(permit.human?).to be_truthy
+        human_permit.save!
+        human_permit.reload
+        expect(human_permit.human?).to be_truthy
       end
       
       it "is impossible when one of required fields is empty" do
         %w[first_name last_name middle_name doc_type doc_number].each do |field|
-          permit.update_attribute(field, nil)
-          permit.save!
-          permit.reload
-          expect(permit.human?).to be_falsey
+          human_permit = FactoryGirl.create(:human_permit)
+          human_permit.update_attribute(field, nil)
+          human_permit.save!
+          human_permit.reload
+          expect(human_permit.human?).to be_falsey
         end
       end
       
       it "is impossible when drive_list exists" do
-        permit.drive_list = true
-        permit.save!
-        permit.reload
-        expect(permit.human?).to be_falsey
+        human_permit.drive_list = true
+        human_permit.save!
+        human_permit.reload
+        expect(human_permit.human?).to be_falsey
       end
     end
-
+  
   end
 
 end
