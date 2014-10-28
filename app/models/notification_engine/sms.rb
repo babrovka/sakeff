37ebbd5d08b3d18047622188748b7b087cc01ae1
Vladimir Krivchenko <vladimir.krivchenko@gmail.com>
2014-10-28 17:27:00 +0300

    Delayed SMS send.

--- app/models/notification_engine/sms.rb
+++ app/models/notification_engine/sms.rb
@@ -2,7 +2,7 @@ module NotificationEngine
   class Sms
     def self.notify user, message, options
       unless user.cell_phone_number.nil? || user.cell_phone_number.empty?
-        Im::SmsPresenter.send_message user.cell_phone_number, URI.encode(message.text)
+        Im::SmsPresenter.delay.send_message user.cell_phone_number, URI.encode(message.text)
       end
     end
   end
